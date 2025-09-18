34ca436388ae01ed67e04d2f5772c79fb4d73ee6
liamw8lde <91687269+liamw8lde@users.noreply.github.com>
2025-09-18 11:41:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 
 # Basic config
-st.set_page_config(page_title="Wochenplan")  # default layout & theme
+st.set_page_config(page_title="Wochenplan", layout="wide", initial_sidebar_state="collapsed")
+
 
 CSV_URL = "https://raw.githubusercontent.com/liamw8lde/Winter-2024_2025-Training-PLan/main/winter_training.csv"
 
@@ -107,3 +108,4 @@ with tab2:
         st.dataframe(pf[["Spieler_Name", "Datum", "Tag", "Slot", "Typ", "Spieler"]], use_container_width=True)
     else:
         st.info("Bitte Spieler auswählen.")
+
